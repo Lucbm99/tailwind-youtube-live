95133a3e4f027144995327c639d8cd5788a6f5a1
Lucbm99 <lbaradelmarchiori@gmail.com>
2021-09-27 21:36:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { TestimonialItem } from "./components/TestimonialItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="p-10">
+      <TestimonialItem />
+
+      <a 
+        className="block shadow bg-green-600 text-white text-center p-4 mt-5 rounded-xl hover:bg-green-500"
+        href="https://b7web.com.br/fullstack">
+        Quero me inscrever no curso
         </a>
-      </header>
     </div>
   );
 }
